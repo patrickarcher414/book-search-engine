047ceacf64c48653dbbf4d99b5a444a9ac015f40
patrickarcher414 <patrickarcher414@gmail.com>
2022-07-23 18:21:53 -0500

    resolvers mutation login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,39 @@
+const { User } = require ("../models")
+const { signToken } = require ("../utils/auth")
+const { AuthenticationError } = require ("apollo-server-express")
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-       return "Hello World!";
+    me: async (parent, args, context) => {
+       if (context.user) {
+        const userData = await User.findOne({_id: context.user._id}).select("-__v -password")
+        return userData
+       }
+       throw new AuthenticationError("Please login.")
+    }
+  },
+  Mutation: {
+    login: async (parent, args) => {
+      const {email, password} = args
+      let user = await User.findOne({email})
+      if (!user) {
+        throw new AuthenticationError("Please enter valid email.")
+      }
+      let correctPW = await user.isCorrectPassword(password)
+      if (!correctPW) {
+        throw new AuthenticationError("Please enter a valid password!")
+      }
+      const token = signToken(user)
+      return { token, user }
+    },
+    addUser: async (parent, args) => {
+      
+    },
+    saveBook: async (parent, args, context) => {
+      
+    },
+    removeBook: async (parent, args, context) => {
+      
     }
   }
 };
